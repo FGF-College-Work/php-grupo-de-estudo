9d19f546454d7ef0eb9127551638a9fe53bb607b
Cleilson <marcialwushu@gmail.com>
2018-01-20 21:15:37 -0200

    Update hello-world.php

---
+++ exemplo-codigo/code snippets/Basic/Hello-World/hello-world.php
@@ -0,0 +1,15 @@
+<?
+
+//Using print() function with parentheses
+print("Hello World<br>");
+
+//Using print() function without parentheses
+print "Hello World<br>";
+
+//Using echo() function with parentheses
+echo("Hello World<br>");
+
+//Using echo() function without parentheses
+echo "Hello World";
+
+?>
