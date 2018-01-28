0edb9dfe15007202568c844182843eca5921c44b
Cleilson <marcialwushu@gmail.com>
2018-01-28 20:18:36 -0200

    Update recursive_function_to_traverse_a_multi-dimensional_array.php
    Php > Algorithms Code Examples
    A recursive function to traverse a multi-dimensional array where the

---
+++ exemplo-codigo/code_sample/Algorithms/recursive_function_to_traverse_a_multi-dimensional_array/recursive_function_to_traverse_a_multi-dimensional_array.php
@@ -0,0 +1,21 @@
+<? 
+#   A recursive function to traverse a multi-dimensional array 
+#   where the dimensions are not known 
+# 
+
+function get_array_elems($arrResult, $where="array"){ 
+           while(list($key,$value)=each($arrResult)){ 
+                 if (is_array($value)){ 
+                    get_array_elems($value, $where."[$key]"); 
+                 } 
+                 else { 
+                       for ($i=0; $i<count($value);$i++){ 
+                             echo $where."[$key]=".$value."<BR>\n"; 
+                       } 
+                 } 
+           } 
+  } 
+
+  get_array_elems($arrResult); 
+
+?> A recursive function to traverse a multi-dimensional array where the
